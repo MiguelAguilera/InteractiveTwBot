430b24f6b95c940810c074d60f3feff71e8566b4
MiguelAguilera <miguel@maguilera.net>
2017-11-18 16:40:40 +0100

    create script for posting status

---
+++ post.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+from twython import Twython
+import random
+
+# Load twitter keys
+keys_file = 'tw.keys'
+with open(keys_file) as f:
+    APP_KEY = f.readline().strip("\n")
+    APP_SECRET = f.readline().strip("\n")
+    OAUTH_TOKEN = f.readline().strip("\n")
+    OAUTH_TOKEN_SECRET = f.readline().strip("\n")
+
+# Create twitter instance
+tw = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+# Load random image and uplaod it to twitter
+num = random.randint(0, 20 - 1)
+imagefile = 'sad-cat/' + str(num) + '.jpg'
+photo = open(imagefile, 'rb')
+image_ids = tw.upload_media(media=photo)
+
+# Write tweet text
+tweet_text = 'Testing Etopia Bot' + str(num)
+
+# Update status to twitter
+tw.update_status(status=tweet_text, media_ids=image_ids['media_id'])
